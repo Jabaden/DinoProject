b99a7b14da6c53bc7ac4f7361394b1ffc5696927
Jabaden <jabaden@mac.com>
2015-03-11 23:52:20 -0700

    monsters attack you.

--- public_html/enemies.js
+++ public_html/enemies.js
@@ -1,14 +1,28 @@
+
 function enemy(game, image, aImage, group)
 {
+    this.enemySwipe;
+    this.attackAgain = true;
     console.log("enemy being called");
     this.badGuy = group.create(game.world.randomX, game.world.randomY, image);
     // bug: this doesn't change the body, only the sprite:
     // this.badGuy.scale.set(.7);
     game.physics.p2.enable(this.badGuy,true);
     game.physics.enable(this.badGuy, Phaser.Physics.ARCADE);
+    
     this.badGuy.body.collideWorldBounds = true;
-    this.enemyCircle = new Phaser.Circle(game.world.centerX, 150,150);
+    this.enemyCircle = new Phaser.Circle(game.world.centerX, 125,125);
+    this.attackCircle = new Phaser.Circle(game.world.centerX, 50,50);
+    this.enemySwipe = game.add.sprite(350,350,aImage);
+    game.physics.p2.enable(this.enemySwipe, false);
+    this.enemySwipe.animations.add('attackSwipe');
     this.badGuy.health = 3;
+    this.badGuy.height = 75;
+    this.badGuy.body.height = 75;
+    this.badGuy.body.width = 30;
+    this.badGuy.width = 30;
+    this.enemySwipe.body.kinematic = true;
+    this.enemySwipe.kill();
     
     this.update = function()
     {
@@ -19,10 +33,13 @@ function enemy(game, image, aImage, group)
     {
         this.enemyCircle.x = this.badGuy.x; 
         this.enemyCircle.y = this.badGuy.y;
+        this.attackCircle.y = this.badGuy.y;
+        this.attackCircle.x = this.badGuy.x;
     }
     this.circleRender = function()
     {
         game.debug.geom(this.enemyCircle,'#cfffff', false);
+        game.debug.geom(this.attackCircle,'#cfffff', false);
         
     }
     
@@ -46,5 +63,45 @@ function enemy(game, image, aImage, group)
     {
         return this.enemyCircle;
     }
+    this.getACircle = function()
+    {
+        return this.attackCircle;
+    }
+    this.getEnemyAttack = function()
+    {
+        return this.enemySwipe;
+    }
+    this.killAttack = function()
+    {
+        console.log("kill attack");
+        this.enemySwipe.kill();
+    }
+    this.reviveAttack = function()
+    {
+        this.enemySwipe.revive();
+    }
+    this.getAttackX = function()
+    {
+        return this.enemySwipe.x;
+    }
+    this.getAttackY = function()
+    {
+        return this.enemySwipe.y;
+    }
+    this.cannotAttack = function()
+    {
+        this.attackAgain = false;
+        console.log("can no longer attack!");
+    }
+    this.canAttack = function()
+    {
+        this.attackAgain = true;
+        console.log("can attack!");
+    }
+    this.queryAttack = function()
+    {
+        return this.attackAgain;
+    }
+    
     
 }   
